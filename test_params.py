2ea3f154308c8dbb38285f7d54dfd20d80061428
Vsevolod Konyakhin <sevakonyakhin@gmail.com>
2020-04-25 05:14:04 +0300

    small change in test params

--- test_params.py
+++ test_params.py
@@ -1,20 +1,21 @@
-import torch
-
 import config as cfg
 from model import EfficientDet
 from utils.utils import count_parameters
 
 """ Quick test on parameters number """
 
-model = EfficientDet.from_pretrained().to('cpu')
+model = EfficientDet.from_pretrained('efficientdet-d0').to('cpu')
 
 model.eval()
-params = count_parameters(model)
 
-print('Model: {}, params: {:.6f}M, params in paper: {}'.format(cfg.MODEL_NAME, params / 1e6,
-                                                     cfg.PARAMS))
-print('   Backbone: {:.6f}M'.format(count_parameters(model.backbone) / 1e6))
-print('   Adjuster: {:.6f}M'.format(count_parameters(model.adjuster) / 1e6))
-print('      BiFPN: {:.6f}M'.format(count_parameters(model.bifpn) / 1e6))
-print('       Head: {:.6f}M'.format((count_parameters(model.classifier) +
-                                    count_parameters(model.regresser)) / 1e6))
+print('Model: {}, params: {:.6f}M, params in paper: {}'.format(
+    cfg.MODEL_NAME, count_parameters(model) / 1e6, cfg.PARAMS))
+print('   Backbone: {:.6f}M'.format(
+    count_parameters(model.backbone) / 1e6))
+print('   Adjuster: {:.6f}M'.format(
+    count_parameters(model.adjuster) / 1e6))
+print('      BiFPN: {:.6f}M'.format(
+    count_parameters(model.bifpn) / 1e6))
+print('       Head: {:.6f}M'.format(
+   (count_parameters(model.classifier) +
+    count_parameters(model.regresser)) / 1e6))
